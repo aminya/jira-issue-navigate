963e9478505688074d3c220f644ce01f92734596
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-13 16:39:12 -0700

    feat: display the url of the button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         jira-issue-navigate
-// @version      0.3.0
+// @version      0.4.0
 // @description  Go to the next issue using a button
 // @author       Amin Yahyaabadi
 // @match        https://*.atlassian.net/browse/*
@@ -28,13 +28,15 @@ function main() {
   const queriesString = queries === undefined ? "" : queries
 
   // create a button to go to the next issue
-  const nextButton = document.createElement("button")
+  const nextButton = document.createElement("a")
   nextButton.id = "next-issue-btn"
   nextButton.setAttribute("aria-label", "Go to next issue")
   nextButton.setAttribute("aria-expanded", "false")
   nextButton.setAttribute("aria-haspopup", "true")
   nextButton.setAttribute("type", "button")
   nextButton.style.borderRadius = "2px"
+  nextButton.style.alignSelf = "center"
+  nextButton.style.padding = "7px"
   const buttonIcon = document.createElement("div")
   buttonIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20.633 20.633" style="enable-background:new 0 0 20.633 20.633" xml:space="preserve">
     <path d="M15.621 9.844 5.971.195A.652.652 0 0 0 5.5 0a.664.664 0 0 0-.473.195l-.013.012a.677.677 0 0 0-.197.475v4.682c0 .178.071.348.197.471l4.481 4.482-4.481 4.479a.667.667 0 0 0-.197.475v4.68c0 .18.071.354.197.475l.013.01a.664.664 0 0 0 .947 0l9.647-9.646a.671.671 0 0 0 0-.946z" />
@@ -81,17 +83,10 @@ function main() {
     nextButton.className = likeButton.className
   }
 
-  // button functionality
-  nextButton.addEventListener("click", () => {
-    // get the next issue number
-    const nextIssueNumber = issueNumber + 1
-
-    // create the next issue url
-    const nextIssueURL = `${company}.atlassian.net/${middle}/${project}-${nextIssueNumber}${queriesString}`
-
-    // navigate to the next issue
-    window.location.href = nextIssueURL
-  })
+  // create the next issue url
+  const nextIssueURL = `${company}.atlassian.net/${middle}/${project}-${issueNumber + 1}${queriesString}`
+  // navigate to the next issue on click
+  nextButton.setAttribute("href", nextIssueURL)
 
   nextButton.addEventListener("mouseover", () => {
     nextButton.style.background = "#091e4214"
