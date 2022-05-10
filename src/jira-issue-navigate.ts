0fe7f3f78c53d6bdb5ee8589b07d2248257a234d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-05-09 17:58:31 -0700

    fix: embed the rightArrow svg using Parcel's bundle-text

--- src/jira-issue-navigate.ts
+++ src/jira-issue-navigate.ts
@@ -12,6 +12,8 @@
 // ==/UserScript==
 
 import { parseIssueUrl } from "./jira-issue-url"
+// @ts-ignore
+import * as rightArrow from "bundle-text:./right-arrow.svg"
 
 function createButton(
   company: string,
@@ -32,9 +34,7 @@ function createButton(
   button.style.alignSelf = "center"
   button.style.padding = "7px"
   const buttonIcon = document.createElement("div")
-  buttonIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20.633 20.633" style="enable-background:new 0 0 20.633 20.633" xml:space="preserve">
-    <path d="M15.621 9.844 5.971.195A.652.652 0 0 0 5.5 0a.664.664 0 0 0-.473.195l-.013.012a.677.677 0 0 0-.197.475v4.682c0 .178.071.348.197.471l4.481 4.482-4.481 4.479a.667.667 0 0 0-.197.475v4.68c0 .18.071.354.197.475l.013.01a.664.664 0 0 0 .947 0l9.647-9.646a.671.671 0 0 0 0-.946z" />
-</svg>`
+  buttonIcon.innerHTML = rightArrow as string
   button.style.background = "none"
   button.style.border = "none"
 
