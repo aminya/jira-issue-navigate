e6f0941bfc4ec693ca407d2220ad8852b3759c5c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-09-19 00:39:25 -0700

    fix: add namespace and homepage to the header

--- src/jira-issue-navigate-header.ts
+++ src/jira-issue-navigate-header.ts
@@ -1,26 +1,22 @@
 import { generate } from "@userscripters/generate-headers/dist/generate.js"
 import { dirname, join } from "path"
-import { readFile, writeFile } from "fs/promises"
 import desm from "desm"
 
 const __dirname = desm(import.meta.url)
 
 async function main() {
-  const content = await generate("tampermonkey", {
+  const file = join(dirname(__dirname), "dist", "jira-issue-navigate.js")
+
+  await generate("tampermonkey", {
     packagePath: join(dirname(__dirname), "package.json"),
-    output: "./a.js",
-    direct: true,
+    output: file,
     matches: ["https://*.atlassian.net/browse/*", "https://*.atlassian.net/jira/software/projects/*"],
+    namespace: "AminYa",
+    homepage: "https://github.com/aminya/jira-issue-navigate",
     run: "end",
     collapse: false,
     eol: "\n",
   })
-
-  const file = join(dirname(__dirname), "dist", "jira-issue-navigate.js");
-
-  // prepend the header to the file
-  const fileContent = await readFile(file, "utf-8")
-  await writeFile(file, `${content}\n${fileContent}`)
 }
 
 main().catch((err) => {
